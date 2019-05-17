cf7db6835fa8eb214baeccb40d2d1eda9e89b6f3
Simon Peterhans <simon.peterhans@gmail.com>
2019-05-17 22:43:42 +0200

    Add generic setup.py.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='tbd',
+    version='0.0.0',
+    description='tba',
+    # long_description=readme,
+    author='tba',
+    author_email='tba',
+    url='tba',
+    # license=license,
+    packages=find_packages(exclude=())
+)
