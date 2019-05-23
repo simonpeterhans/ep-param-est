cc0c7c1d62d3c17ab1eeb1e100f2a82f9c79ed1e
Simon Peterhans <simon.peterhans@gmail.com>
2019-05-23 09:47:10 +0200

    Add path to main.

--- param_est/__main__.py
+++ param_est/__main__.py
@@ -1,7 +1,13 @@
+import os
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from datetime import datetime
 
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter, epilog='tba')
 
+parser.add_argument('--dist', metavar='name', default='norm', type=str, required=True,
+                    help='the distribution to sample the grid from')
+parser.add_argument('--dist-args', metavar='par', nargs='*',
+                    help='the parameters for the selected distribution')
 parser.add_argument('--n-samples', metavar='n', default=1000000,
                     help='number of samples to train the model on')
 parser.add_argument('--grid-size', metavar='x', default=500, help='the size of the grid')
@@ -14,14 +20,18 @@ parser.add_argument('--dense-factor', metavar='f', default=1.0,
 parser.add_argument('--depth', metavar='x', default=25, help='depth of the model')
 parser.add_argument('--kernels', metavar='n', default=25, help='number of kernels to use')
 parser.add_argument('--kernel-size', metavar='x', default=25, help='size of the kernels')
-parser.add_argument('--dist', metavar='name', default='norm', type=str,
-                    help='the distribution to sample the grid from')
-parser.add_argument('--dist-args', metavar='par', nargs='*',
-                    help='the parameters for the selected distribution')
 parser.add_argument('--store-params', action='store_true', help='saves the parameters used')
 parser.add_argument('--store-model', action='store_true', help='saves the model')
 parser.add_argument('--plot-model', action='store_true', help='saves the model as a .png file')
 
 args = parser.parse_args()
 
-print(args)
+# TODO This is probably redundant?
+if __name__ == '__main__':
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    run_name = args.dist
+
+    # TODO Decide where to store output.
+    path = os.path.join(os.getcwd(), 'output', run_name + '-' + now)
+
+    # create_run() ...
